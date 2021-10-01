9497fec109848a4d0f0a7402d34cfc1d732fe164
liviabelirocha <liviabelirocha@outlook.com>
2021-10-01 14:39:06 -0300

    Adding web server

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from decouple import config
 
+from web_server import keep_alive
+
 import Music
 
 cogs = [Music]
@@ -11,5 +13,7 @@ client = commands.Bot(command_prefix="!", intents=intents)
 
 for cog in cogs:
     cog.setup(client)
+
+keep_alive()
 
 client.run(config("DISCORD_PRIVATE_KEY"))
