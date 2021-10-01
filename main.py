72e78d514e37000476ea96266d245e5cbd767340
liviabelirocha <liviabelirocha@outlook.com>
2021-10-01 13:37:39 -0300

    Changing prefix to !

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import Music
 cogs = [Music]
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="$", intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 for cog in cogs:
     cog.setup(client)
