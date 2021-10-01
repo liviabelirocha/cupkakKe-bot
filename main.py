dea5682dca46b0133db2c471f2e5352b7dab5f43
liviabelirocha <liviabelirocha@outlook.com>
2021-10-01 14:56:17 -0300

    config

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext import commands
 from decouple import config
 
-from web_server import keep_alive
-
 import Music
 
 cogs = [Music]
@@ -13,7 +11,5 @@ client = commands.Bot(command_prefix="!", intents=intents)
 
 for cog in cogs:
     cog.setup(client)
-
-keep_alive()
 
 client.run(config("DISCORD_PRIVATE_KEY"))
